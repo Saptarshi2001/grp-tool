85cf17d06df06ebc0ae273c1331418fd858b80b4
Saptarshi Dutta <saptarshidutta2001@gmail.com>
2024-06-23 22:19:55 +0530

    Editing the README.md

--- src/regex.h
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void match(char*,FILE *);
-int matchdigit(char*,FILE *);
-int matchallexpr(char*,FILE *);
-int matchchar(char*,FILE *);
-int matchbeg(char*,FILE *);
--- src/test.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_H_INCLUDED
-#define TEST_H_INCLUDED
-
-void test_beg();
-
-#endif // TEST_H_INCLUDED
